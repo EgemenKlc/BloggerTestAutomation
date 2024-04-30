8b3a5a38d31d5493573ac5f5fa4adb5184933967
EgemenKlc <egemen-kilic@outlook.com>
2024-04-30 12:14:00 +0300

    Azure deploy test2

--- Configurations/Config.py
+++ Configurations/Config.py
@@ -13,7 +13,7 @@ user_mail = "automationblogger5@gmail.com"
 user_password = "adminTest123"
 guest_mail = "guestautomated1@gmail.com"
 guest_password = "Guest123"
-
+##
 # ---------- Url's---------------
 admin_blog_Url = "https://www.blogger.com/"
 guest_blog_Url = "https://bloggerautomation5.blogspot.com/"  # "https://berfintng.blogspot.com/"
